c293d79433c809bde88cfff7f5a493dd84a631ce
Michael de Silva <michael@mwdesilva.com>
2015-03-27 00:07:32 +0530

    Add example rule set

--- lib/alphasights/email_predictor/rule.rb
+++ lib/alphasights/email_predictor/rule.rb
@@ -12,6 +12,24 @@ module Alphasights
         @conditions = conditions
       end
 
+      # NOTE Example set of parsed rules
+      #
+      # [#<Alphasights::EmailPredictor::Rule:0x007f8382c78118
+      #   @conditions=[{:first_name=>/[\w]+/}, {:last_name=>/[\w]+/}],
+      #   @separators=["."],
+      #   @signature=:first_name_dot_last_name>,
+      #  #<Alphasights::EmailPredictor::Rule:0x007f83840c7ce0
+      #   @conditions=[{:first_name=>/[\w]+/}, {:last_initial=>/[\w]/}],
+      #   @separators=["."],
+      #   @signature=:first_name_dot_last_initial>,
+      #  #<Alphasights::EmailPredictor::Rule:0x007f83840c7538
+      #   @conditions=[{:first_initial=>/[\w]/}, {:last_name=>/[\w]+/}],
+      #   @separators=["."],
+      #   @signature=:first_initial_dot_last_name>,
+      #  #<Alphasights::EmailPredictor::Rule:0x007f83840c6db8
+      #   @conditions=[{:first_initial=>/[\w]/}, {:last_initial=>/[\w]/}],
+      #   @separators=["."],
+      #   @signature=:first_initial_dot_last_initial>]
       def self.build
         Configuration.patterns.each do |pattern|
           collection.add BuildExpectation.from_pattern(pattern)
